5ca340bd657a3d500c1c9e45c687574963c337e7
091810467 angel <angelht121018@gmail.com>
2022-03-11 19:47:14 -0600

    Primer Screenshot del Proyecto GitMundo "091810467 angel"

---
+++ index.php
@@ -0,0 +1,35 @@
+
+	<meta charset="UTF-8">
+	<title>Pagina Principal</title>
+</head>
+<body>
+	<h1>Saludos cordiales </h1>
+
+	<div id="newsletter" class="section">
+			<!-- container -->
+			<div class="container">
+				<!-- row -->
+				<div class="row">
+					<div class="col-md-12">
+						<div class="newsletter">
+							<p>Regalanos Tu <strong>COMENTARIO</strong></p>
+							<form>
+								<input class="input" type="text" placeholder="Escribe tu comentario">
+								<button class="newsletter-btn"><i class="fa fa-envelope"></i> comentar</button>
+							</form>
+						
+							
+						</div>
+					</div>
+				</div>
+				<!-- /row -->
+			</div>
+			<!-- /container -->
+		</div>
+</body>
+</html>
+	<meta charset="UTF-8">
+	<title>Pagina Principal</title>
+</head>
+
+</html>
